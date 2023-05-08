3675f47f52343e970b79af470e04d3c3153d9eca
ekialfani <ekialfani15@gmail.com>
2023-05-08 20:53:39 +0800

    style: detail page

--- src/pages/detail/[id].js
+++ src/pages/detail/[id].js
@@ -1,26 +1,29 @@
 import { BsBookmark } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
+import Navigation from "@/components/Navigation";
 
 export default function Detail() {
   return (
-    <div>
+    <>
+    <Navigation />
+      <div className="max-w-3xl mx-auto my-16">
         <img 
           src="https://s.yimg.com/uu/api/res/1.2/LKRH31mzL9wqtcqoQ_lkjw--~B/Zmk9ZmlsbDtoPTYzMDtweW9mZj0wO3c9MTIwMDthcHBpZD15dGFjaHlvbg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2023-04/835a5670-e5f4-11ed-9db6-3febf57b7a4a.cf.jpg" 
-          alt="" 
+          alt=""
         />
         <div className="relative">
-          <h1>
+          <h1 className="text-3xl font-semibold w-4/5 mt-6">
             The second-gen Apple Pencil drops to $89, plus the rest of the week's best tech deals
           </h1>
-          <div>
+          <div className="text-slate-500 flex items-center text-sm mt-3">
             <span>CNN Indonesia</span>
             <RxDotFilled />
             <span>May 6, 2023</span>
           </div>
-          <button>
+          <button className="absolute top-2 right-0 text-2xl">
             <BsBookmark />
           </button>
-          <div>
+          <div className="mt-10 text-slate-600">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
@@ -34,5 +37,6 @@ export default function Detail() {
           </div>
         </div>
     </div>
+    </>
   )
 }
