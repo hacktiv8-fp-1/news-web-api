e4b5b0deadedff25e82923264e6adb3b4af8eb91
Dimas Bayu Pratama <dimasbayu6401@gmail.com>
2023-05-10 15:10:12 +0700

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- src/components/Card/CardItem/index.js
+++ src/components/Card/CardItem/index.js
@@ -20,22 +20,26 @@ function NewsItem({ news }) {
   };
 
   return (
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <img class="rounded-t-lg" src={news?.urlToImage} alt={news?.title} />
+        <img
+          className="rounded-t-lg"
+          src={news?.urlToImage}
+          alt={news?.title}
+        />
       </a>
-      <div class="p-5 relative">
+      <div className="p-5 relative">
         <div className="flex items-center mb-5 text-slate-500 text-sm">
           <span>{news?.author}</span>
           <RxDotFilled />
           <span>{convertDate(news?.publishedAt)}</span>
         </div>
         <a href="#">
-          <h5 class="mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {news?.title}
           </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-600 dark:text-gray-400">
+        <p className="mb-3 font-normal text-gray-600 dark:text-gray-400">
           {news?.description}
         </p>
 
