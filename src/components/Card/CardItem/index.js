04eb44c618dab16eb2f5304e57ad7f5aaa626aad
ekialfani <ekialfani15@gmail.com>
2023-05-11 15:53:19 +0800

    fix: destination address on the link card component and line clamp

--- src/components/Card/CardItem/index.js
+++ src/components/Card/CardItem/index.js
@@ -15,18 +15,17 @@ export default function CardItem({ news, button }) {
         />
       </Link>
       <div class="p-5 relative">
-        <div className="flex items-center mb-5 text-slate-500 text-sm">
-          <span className="line-clamp-1 w-1/2">{news?.author}</span>
+        <div className="flex items-center mb-5 text-[#617784] text-sm w-11/12">
+          <span className="line-clamp-1">{news?.author}</span>
           <RxDotFilled />
-          <span>{convertDate(news?.publishedAt)}</span>
-
+          <span className="line-clamp-1">{convertDate(news?.publishedAt)}</span>
         </div>
-        <Link href={news.url} target="_blank">
+        <Link href={`/detail/${news.title}`}>
           <h5 class="mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-2">
             {news?.title}
           </h5>
         </Link>
-        <p class="mb-3 font-normal text-gray-600 dark:text-gray-400 line-clamp-3">
+        <p class="mb-3 font-normal text-[#617784] dark:text-gray-400 line-clamp-2">
           {news?.description}
         </p>
         {button}
