82c3c9e07f07bdb7a1bd7684a56cd17642991156
ekialfani <ekialfani15@gmail.com>
2023-05-11 18:32:20 +0800

    fix: destination address on the link card component

--- src/components/Card/CardItem/index.js
+++ src/components/Card/CardItem/index.js
@@ -7,7 +7,7 @@ export default function CardItem({ news, button }) {
     <div
       class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
     >
-      <Link href={`/detail/${news.title}`}>
+      <Link href={news.url} target="_blank" rel="noreferrer">
         <img
           class="rounded-t-lg max-h-44 w-full object-cover"
           src={news?.urlToImage}
@@ -20,7 +20,7 @@ export default function CardItem({ news, button }) {
           <RxDotFilled />
           <span className="line-clamp-1">{convertDate(news?.publishedAt)}</span>
         </div>
-        <Link href={`/detail/${news.title}`}>
+        <Link href={news.url} target="_blank" rel="noreferrer">
           <h5 class="mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-2">
             {news?.title}
           </h5>
--- src/pages/detail/[id].js
+++ src/pages/detail/[id].js
@@ -1,39 +1,41 @@
 import { BsBookmark } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 import Navigation from "@/components/Navigation";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { convertDate } from "@/utils/Date";
 
 export default function Detail() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const allNews = useSelector((state) => state.news);
+  const news = allNews.data.find((news) => news.title === id);
+
+  console.log(console.log(news));
+
   return (
     <>
     <Navigation />
       <div className="px-5 sm:px-12 md:max-w-3xl mx-auto my-16">
         <img 
-          src="https://s.yimg.com/uu/api/res/1.2/LKRH31mzL9wqtcqoQ_lkjw--~B/Zmk9ZmlsbDtoPTYzMDtweW9mZj0wO3c9MTIwMDthcHBpZD15dGFjaHlvbg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2023-04/835a5670-e5f4-11ed-9db6-3febf57b7a4a.cf.jpg" 
-          alt=""
+          src={news.urlToImage} 
+          alt={news.title}
         />
         <div className="relative">
           <h1 className="text-2xl md:text-3xl font-semibold w-4/5 mt-6">
-            The second-gen Apple Pencil drops to $89, plus the rest of the week's best tech deals
+            {news.title}
           </h1>
           <div className="text-slate-500 flex items-center text-sm mt-3">
-            <span>CNN Indonesia</span>
+            <span>{news.author}</span>
             <RxDotFilled />
-            <span>May 6, 2023</span>
+            <span>{convertDate(news.publishedAt)}</span>
           </div>
           <button className="absolute top-2 right-0 text-2xl">
             <BsBookmark />
           </button>
           <div className="mt-10 text-slate-600">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros lorem, posuere ac aliquam et, convallis ut neque. In gravida neque leo. Nunc laoreet posuere tortor nec finibus. Vestibulum quis risus dignissim, vehicula magna sit amet, auctor sem. Sed semper mi sit amet tellus pulvinar, at lacinia ligula ornare. Nulla ultrices turpis eget mauris finibus, in varius turpis ultrices. Vivamus aliquam turpis accumsan, aliquet nulla a, hendrerit mi. Nunc nec tincidunt nunc. Donec egestas mattis velit eu condimentum. Nulla vulputate ex et aliquet convallis. Fusce felis velit, imperdiet in egestas id, vulputate eu dolor. Curabitur quis tortor scelerisque felis ornare vulputate eget nec turpis. Nulla lobortis vulputate cursus. Duis luctus orci massa, vel dictum augue ultricies in. Fusce non massa sed augue feugiat accumsan. In malesuada nulla ut nisl placerat commodo.</p>
+            <p>{news.content}</p>
           </div>
         </div>
     </div>
